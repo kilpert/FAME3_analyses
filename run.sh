077376b7393e8858b43296fb64cf3a620c39b3ab
Fabian Kilpert <fkilpert@gmail.com>
2024-08-26 10:06:24 +0200

    add command line for demo as default to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 ## snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete #--rerun-trigger mtime
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/FAME3.config.yaml
+## snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/FAME3.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/demo.config.yaml
 
